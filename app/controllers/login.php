bfac28d279fa8da9a60dc77da05b461c058f4608
charlyecho <cem8887@gmail.com>
2017-12-20 11:18:44 +0100

    login

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -4,8 +4,30 @@ class ControllersLogin {
     public static function home() {
 
         if (!empty($_POST)) {
-            trace("test");
-            die();
+            $username = $_POST["login"];
+            $password = $_POST["password"];
+            $config = require __DIR__."/../../config.php";
+            if ($config["auth_type"] == "ldap") {
+                $_config = array(
+                    'domain_controllers'    => $config["ldap"]["domain"],
+                    'base_dn'               => $config["ldap"]["base_dn"],
+                    'admin_username'        => $config["ldap"]["user"],
+                    'admin_password'        => $config["ldap"]["password"],
+                );
+                $ad = new \Adldap\Adldap();
+                $ad->addProvider($_config);
+
+                try {
+                    // If a successful connection is made to your server, the provider will be returned.
+                    $provider = $ad->connect();
+                    $results = $provider->search()->find("charly");
+                    trace($results);
+                }
+                catch (\Adldap\Auth\BindException $e) {
+                    trace($e);
+                    die();
+                }
+            }
         }
 
         $template = ClassesTwig::getInstance();
