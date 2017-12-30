8a94fc77e8b89bc86c68e53e3920ac405a47d12b
charly <cem8887@gmail.com>
2017-12-30 18:25:47 +0100

    language

--- app/home/controllers/home.php
+++ app/home/controllers/home.php
@@ -11,12 +11,6 @@ class HomeControllersHome {
         if ($user = HelpersUser::getCurrent()) {
             $db = ClassesDb::getInstance();
 
-            // nb rss items unread
-            $sql = "SELECT COUNT(si.id) FROM subscription_item si INNER JOIN subscription s ON s.id = si.subscription_id WHERE s.user_id = ".$db->quote($user->id);
-            $s = $db->prepare($sql);
-            $s->execute();
-            $nb_rss = $s->fetch(PDO::FETCH_COLUMN);
-
             $sql = "SELECT COUNT(si.id) FROM subscription_item si INNER JOIN subscription s ON s.id = si.subscription_id WHERE si.read = 0 AND s.user_id = ".$db->quote($user->id);
             $s = $db->prepare($sql);
             $s->execute();
@@ -30,9 +24,9 @@ class HomeControllersHome {
 
             $template = ClassesTwig::getInstance();
             return $template->render("home/views/home.twig", array(
+                "date" => ClassesDate::getInstance()->format("Y-m-d"),
                 "nb_links" => $nb_links,
                 "nb_unread" => $nb_unread,
-                "nb_rss" => $nb_rss
             ));
         }
 
